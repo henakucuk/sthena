fc4be5ad6603c1fcf800a3952615c2fde717d8e8
hena <132471840+henakucuk@users.noreply.github.com>
2024-01-06 19:05:07 +0300

    Add files via upload

---
+++ 1_main.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+st.title("WELCOME")
+st.header("YOU'LL KNOW HENA")
+st.subheader("GET READY")
+
+st.write("are you ready?")
+
+st.write("*firstly:*")
+st.write("**go to the page named: whoishena**")
+st.write("***and then read***")
+
+st.write("- i hope u'll have fun")
+st.write("- love ya")
+
+st.image(image="https://avatars.githubusercontent.com/u/132471840?s=96&v=4",
+         caption="my photo")
+
+st.write("eposta = henaakucuk@gmail.com")
+st.write("[My GitHub](https://github.com/henakucuk)")
